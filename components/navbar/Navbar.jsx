629945bc10fadce3658474e452ab525428b25d89
MeetM15 <meetmistry1510@gmail.com>
2022-01-08 00:27:23 +0530

    added dropdown

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   BellIcon,
   ChevronDownIcon,
@@ -6,9 +5,13 @@ import {
 } from "@heroicons/react/solid";
 import { Menu, Transition } from "@headlessui/react";
 import { useRouter } from "next/router";
-
+import { useState } from "react";
 const Navbar = ({ user, setToggleLoginModalOpen, setShowWalletModal }) => {
   const router = useRouter();
+  const [currency, setCurrency] = useState([
+    "Ethereum",
+    "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/13c43/eth-diamond-black.png",
+  ]);
   return (
     <nav className="bg-secondary relative w-full shadow px-2 sm:px-6 lg:px-8 flex items-center justify-between h-12 text-white">
       <div className="flex-shrink-0 flex items-center">
@@ -27,7 +30,18 @@ const Navbar = ({ user, setToggleLoginModalOpen, setShowWalletModal }) => {
         <Menu as="div" className="relative px-2 py-1">
           <div>
             <Menu.Button className="flex items-center justify-between text-sm text-white">
-              <span className="text-md font-medium">Currency</span>
+              {currency[0] && currency[1] ? (
+                <>
+                  <span className="text-md font-medium">{currency[0]}</span>
+                  <img
+                    src={currency[1]}
+                    alt="ethereum"
+                    className="ml-2 p-1 w-5"
+                  />
+                </>
+              ) : (
+                ""
+              )}
               <ChevronDownIcon className="ml-2 h-6 w-6 opacity-60" />
             </Menu.Button>
           </div>
@@ -38,45 +52,25 @@ const Navbar = ({ user, setToggleLoginModalOpen, setShowWalletModal }) => {
             leave="transition-opacity ease-linear duration-100"
             leaveFrom="opacity-100"
             leaveTo="opacity-0">
-            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 ">
+            <Menu.Items className="origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 ">
               <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={
-                      active
-                        ? "bg-gray-100"
-                        : "block px-4 py-2 text-sm text-gray-700"
-                    }>
-                    Your Profile
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={
-                      active
-                        ? "bg-gray-100"
-                        : "block px-4 py-2 text-sm text-gray-700"
-                    }>
-                    Settings
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={
-                      active
-                        ? "bg-gray-100"
-                        : "block px-4 py-2 text-sm text-gray-700"
-                    }>
-                    Sign out
-                  </a>
-                )}
+                <span
+                  className={
+                    "px-4 py-2 text-sm text-gray-700 flex cursor-pointer"
+                  }
+                  onClick={() =>
+                    setCurrency([
+                      "Ethereum",
+                      "https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/13c43/eth-diamond-black.png",
+                    ])
+                  }>
+                  Ethereum
+                  <img
+                    src="https://ethereum.org/static/6b935ac0e6194247347855dc3d328e83/13c43/eth-diamond-black.png"
+                    alt="ethereum"
+                    className="ml-2 p-1 w-5"
+                  />
+                </span>
               </Menu.Item>
             </Menu.Items>
           </Transition>
@@ -106,33 +100,27 @@ const Navbar = ({ user, setToggleLoginModalOpen, setShowWalletModal }) => {
               leave="transition ease-in duration-75"
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95">
-              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 ">
+              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-32 rounded-md shadow-lg py-2 bg-white ring-1 ring-black ring-opacity-5 ">
                 <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={
-                        active
-                          ? "bg-gray-100"
-                          : "block px-4 py-2 text-sm text-gray-700"
-                      }
-                      onClick={() => {
-                        localStorage.removeItem("token");
-                        router.reload();
-                      }}>
-                      Sign out
-                    </a>
-                  )}
+                  <a
+                    href="#"
+                    className={"px-4 py-2 text-sm text-gray-700"}
+                    onClick={() => {
+                      localStorage.removeItem("token");
+                      router.reload();
+                    }}>
+                    Sign out
+                  </a>
                 </Menu.Item>
               </Menu.Items>
             </Transition>
           </Menu>
-          <button type="button" className="p-1 rounded-full hover:text-white ">
+          {/* <button type="button" className="p-1 rounded-full hover:text-white ">
             <BellIcon
               className="h-6 w-6 text-white opacity-60"
               aria-hidden="true"
             />
-          </button>
+          </button> */}
 
           {/* Profile dropdown */}
         </div>
